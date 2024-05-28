2abd4315b015437a2117c833e80a70815d2f6fac
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2024-05-29 07:23:46 +0900

    setup project

---
+++ Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LeaveMgmt.App.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
